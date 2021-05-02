d92e47d9436276e65a5f6b7fd07a1ad716eb8486
Documenter.jl <documenter@juliadocs.github.io>
2021-05-02 01:02:19 +0000

    build based on 5ba5666

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#InventoryManagement.jl","page":"Home","title":"InventoryManagement.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InventoryManagement.jl","category":"page"}]
+}
