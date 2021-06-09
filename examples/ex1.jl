2996178a2fa2b50d7477ad55fcd413e551ef8404
Hector Perez <hdperez@cmu.edu>
2021-06-09 10:53:38 -0400

    Update ex1.jl
    make inputs more intuitive

--- examples/ex1.jl
+++ examples/ex1.jl
@@ -16,31 +16,30 @@ set_prop!(net, :bill_of_materials, bom)
 set_props!(net, 1, Dict(:initial_inventory => Dict(:A => 75, :B => 100),
                         :inventory_capacity => Dict(:A => Inf, :B => Inf),
                         :holding_cost => Dict(:A => 0, :B => 0),
-                        :production_cost => Dict(:A => 0.01, :B => 0),
-                        :production_time => Dict(:A => 0, :B => 0),
-                        :production_capacity => Dict(:A => Inf, :B => 0)))
+                        :production_cost => Dict(:A => 0.01),
+                        :production_time => Dict(:A => 0),
+                        :production_capacity => Dict(:A => Inf)))
 
-set_props!(net, 2, Dict(:initial_inventory => Dict(:A => 125, :B => 0),
-                        :inventory_capacity => Dict(:A => Inf, :B => Inf),
-                        :holding_cost => Dict(:A => 0, :B => 0)))
+set_props!(net, 2, Dict(:initial_inventory => Dict(:A => 125),
+                        :inventory_capacity => Dict(:A => Inf),
+                        :holding_cost => Dict(:A => 0)))
 
-set_props!(net, 3, Dict(:initial_inventory => Dict(:A => 100, :B => 0),
-                        :inventory_capacity => Dict(:A => Inf, :B => Inf),
-                        :holding_cost => Dict(:A => 0.01, :B => 0),
-                        :demand_distribution => Dict(:A => Normal(5,0.5),
-                                                     :B => [0]),
-                        :demand_frequency => Dict(:A => 0.5, :B => 0),
-                        :sales_price => Dict(:A => 3, :B => 0),
-                        :demand_penalty => Dict(:A => 0.01, :B => 0),
-                        :supplier_priority => Dict(:A => [1,2], :B => [1,2])))
+set_props!(net, 3, Dict(:initial_inventory => Dict(:A => 100),
+                        :inventory_capacity => Dict(:A => Inf),
+                        :holding_cost => Dict(:A => 0.01),
+                        :demand_distribution => Dict(:A => Normal(5,0.5)),
+                        :demand_frequency => Dict(:A => 0.5),
+                        :sales_price => Dict(:A => 3),
+                        :demand_penalty => Dict(:A => 0.01),
+                        :supplier_priority => Dict(:A => [1,2])))
 
 #specify sales prices, transportation costs, lead time
-set_props!(net, 1, 3, Dict(:sales_price => Dict(:A => 2, :B => 0),
-                          :transportation_cost => Dict(:A => 0.01, :B => 0),
+set_props!(net, 1, 3, Dict(:sales_price => Dict(:A => 2),
+                          :transportation_cost => Dict(:A => 0.01),
                           :lead_time => Poisson(3)))
 
-set_props!(net, 2, 3, Dict(:sales_price => Dict(:A => 1, :B => 0),
-                          :transportation_cost => Dict(:A => 0.01, :B => 0),
+set_props!(net, 2, 3, Dict(:sales_price => Dict(:A => 1),
+                          :transportation_cost => Dict(:A => 0.01),
                           :lead_time => Poisson(7)))
 
 #create environment
@@ -50,8 +49,8 @@ env = SupplyChainEnv(net, num_periods)
 #define reorder policy parameters
 policy = :sS #(s, S) policy
 on = :position #monitor inventory position
-s = Dict((3,:A) => 50, (3,:B) => 0) #lower bound on inventory
-S = Dict((3,:A) => 100, (3,:B) => 0) #base stock level
+s = Dict((3,:A) => 50) #lower bound on inventory
+S = Dict((3,:A) => 100) #base stock level
 
 #run simulation with reorder policy
 simulate_policy!(env, s, S, on, policy, :priority)
