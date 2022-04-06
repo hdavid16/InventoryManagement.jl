79495940ffc069d4ac47c5d80f63f53563881d2a
hdavid16 <hperez16@gmail.com>
2022-04-06 15:15:02 -0400

    upgrade policy code (use topological sorting to place orders) this allows placing orders moving upstream in the network closes Need effective echelon stock #67

--- src/policy.jl
+++ src/policy.jl
@@ -1,5 +1,5 @@
 """
-    reorder_policy(env::SupplyChainEnv, param1::Dict, param2::Dict,
+    reorder_policy(env::SupplyChainEnv, reorder_point::Dict, policy_param::Dict,
                         policy_type::Union{Dict, Symbol} = :rQ, 
                         review_period::Union{Int, StepRange, Vector, Dict} = 1,
                         min_order_qty::Union{Real, Dict} = 0)
@@ -9,50 +9,33 @@ Apply an inventory policy to specify the replinishment orders for each material
 
 # Arguments
 - `env::SupplyChainEnv`: inventory management environment
-- `param1::Dict`: the `s` or `r` parameter in each node for each material in the system. The `keys` are of the form `(node, material)`.
-- `param2::Dict`: the `S` or `Q` parameter in each node for each material in the system. The `keys` are of the form `(node, material)`.
+- `reorder_point::Dict`: the `s` or `r` parameter in each node for each material in the system. The `keys` are of the form `(node, material)`.
+- `policy_param::Dict`: the `S` or `Q` parameter in each node for each material in the system. The `keys` are of the form `(node, material)`.
 - `policy_type::Union{Symbol, Dict}`: `:rQ` for an `(r,Q)` policy, or `:sS` for an `(s,S)` policy. If passing a `Dict`, the policy type should be specified for each node (keys).
 - `review_period::Union{Int, StepRange, Vector, Dict}`: number of periods between each inventory review (Default = `1` for continuous review.). If a `StepRange` or `Vector` is used, the `review_period` indicates which periods the review is performed on. If a `Dict` is used, the review period should be specified for each `(node, material)` `Tuple` (keys). The values of this `Dict` can be either `Int`, `StepRange`, or `Vector`. Any missing `(node, material)` key will be assigned a default value of 1.
 - `min_order_qty::Union{Real, Dict}`: minimum order quantity (MOQ) at each supply node. If a `Dict` is passed, the MOQ should be specified for each `(node, material)` `Tuple` (keys). The values should be `Real`. Any missing key will be assigned a default value of 0.
 """
-function reorder_policy(env::SupplyChainEnv, param1::Dict, param2::Dict;
+function reorder_policy(env::SupplyChainEnv, reorder_point::Dict, policy_param::Dict;
                         policy_variable::Union{Dict,Symbol} = :inventory_position,
                         policy_type::Union{Dict, Symbol} = :rQ, 
                         review_period::Union{Int, StepRange, Vector, Dict} = 1,
                         min_order_qty::Union{Real, Dict} = 0)
 
-    #check review period
+    #check review period; if not in review period, send null action
     null_action = zeros(length(env.materials)*ne(env.network))
-    if review_period isa Int && !iszero(mod(env.period,review_period)) #if not in review period, send null action
-        return null_action
-    elseif review_period isa Union{StepRange,Vector} && !in(env.period, review_period)
-        return null_action
-    end
+    !isvalid_period(env.period, review_period) && return null_action
 
     #read parameters
-    nodes = [n for n in vertices(env.network) if !isempty(inneighbors(env.network, n))] #all non-source nodes can place orders
-    supply_nodes = [n for n in vertices(env.network) if !isempty(outneighbors(env.network, n))] #all non-sink nodes can place orders
+    nodes = reverse(topological_sort_by_dfs(env.network)) #sort nodes in reverse topological order so that orders are placed moving up the network
+    source_nodes = filter(n -> isempty(inneighbors(env.network, n)), vertices(env.network)) #source nodes (can't place replenishment orders)
+    sink_nodes = filter(n -> isempty(outneighbors(env.network, n)), vertices(env.network)) #sink nodes (can't fulfill replenishment orders)
+    request_nodes = setdiff(nodes, source_nodes) #nodes placing requests (all non-source nodes)
+    supply_nodes = setdiff(nodes, sink_nodes) #nodes fulfilling requests (all non-sink nodes; used for MOQ)
     arcs = [(e.src, e.dst) for e in edges(env.network)]
     mats = env.materials
 
     #check inputs
-    for p in mats
-        for n in nodes
-            for param in [param1, param2] 
-                if !in((n,p), keys(param)) 
-                    param[(n,p)] = -1 #if no policy given for a node/material, set the params to -1 so that a reorder is never triggered
-                end
-            end
-            if review_period isa Dict && !in((n,p), keys(review_period))
-                review_period[(n,p)] = 1 #set to default value of 1
-            end
-        end
-        for n in supply_nodes
-            if min_order_qty isa Dict && !in((n,p), keys(min_order_qty))
-                min_order_qty[(n,p)] = 0 #set to default value of 0
-            end
-        end
-    end
+    check_policy_inputs!(reorder_point, policy_param, review_period, min_order_qty, mats, request_nodes, supply_nodes)
 
     #filter data for policy
     state1_df = filter(:period => j -> j == env.period, env.inventory_position, view=true) #inventory position
@@ -60,50 +43,143 @@ function reorder_policy(env::SupplyChainEnv, param1::Dict, param2::Dict;
     state2_df = filter(:period => j -> j == env.period, env.echelon_stock, view=true) #inventory position
     state2_grp = groupby(state2_df, [:node, :material]) #group by node and material
 
-    #create action matrix
-    action = zeros(length(mats), length(arcs)) #initialize action matrix
-    for n in nodes, (k, p) in enumerate(mats)
-        #if trigger level is negative, skip it
-        param1[n,p] < 0 && continue 
-        #if not a review period, skip
-        if review_period isa Dict
-            review_period[n,p] isa Int && !iszero(mod(env.period, review_period[n,p])) && continue #trigger if not in review period
-            review_period[n,p] isa Union{StepRange,Vector} && !in(env.period, review_period[n,p]) && continue
+    #initialize action matrix
+    action = NamedArray(
+        zeros(length(mats), length(arcs)),
+        (mats, arcs),
+        (:material, :arc)
+    )
+    for n in request_nodes, mat in mats
+        #check if an order can be placed
+        reorder_point[n,mat] < 0 && continue #if trigger level is negative, skip it
+        review_period isa Dict && !isvalid_period(env.period, review_period[n,mat]) && continue #if not a review period, skip
+        #review inventory at the node
+        state = get_inventory_state(n, mat, policy_variable, state1_grp, state2_grp)
+        #if n is a plant, adjust the inventory state by the order from the downstream node
+        if n in env.producers
+            state += get_expected_consumption(env, n, mat, action)
         end
-        #get MOQ
-        supplier = get_prop(env.network, n, :supplier_priority)[p][1] #get first priority supplier
-        MOQ = min_order_qty isa Real ?
-                min_order_qty :
-                min_order_qty[(supplier,p)]
-        #initialize reorder quantity
-        reorder = 0
-        #check if reorder is triggered & set reorder policy
-        pol_var = policy_variable isa Dict ? policy_variable[n] : policy_variable
-        if pol_var == :inventory_position
-            state = state1_grp[(node = n, material = p)].level[1]
-        elseif pol_var == :echelon_stock
-            state = state2_grp[(node = n, material = p)].level[1]
-        else
-            @assert pol_var in [:inventory_position, :echelon_stock] "Policy variable must be either `:inventory_position` or `:echelon_stock`."
+        #check if reorder is triggered & calculate quantity
+        if state <= reorder_point[n,mat]
+            supplier = get_prop(env.network, n, :supplier_priority)[mat][1] #get first priority supplier
+            MOQ = min_order_qty isa Real ? min_order_qty : min_order_qty[(supplier,mat)] #minimum order quantity
+            action[mat, (supplier,n)] = calculate_reorder(n, mat, state, MOQ, policy_type, policy_param)
         end
-        if state <= param1[n,p]
-            pol_type = policy_type isa Dict ? policy_type[n] : policy_type
-            if pol_type == :rQ #rQ policy
-                reorder = max(param2[n,p], MOQ)
-            elseif pol_type == :sS #sS policy
-                reorder = state < param2[n,p] ? max(param2[n,p] - state, MOQ) : 0
-            else
-                @assert pol_type in [:rQ, :sS] "The policy type must be either `:rQ` or `:sS`."
-            end
-        else
-            continue
-        end
-        #assign reorder quantity
-        j = findfirst(i -> i == (supplier, n), arcs) #find index in action matrix
-        action[k, j] = reorder #sate request quantity
     end
 
     return collect(Iterators.flatten(action))
+end
+
+"""
+    isvalid_period(period::Int, review_period::Union{Int, StepRange, Vector, Dict})
+
+Check if current period is a review period.
+"""
+function isvalid_period(period::Int, review_period::Union{Int, StepRange, Vector, Dict})
+    if review_period isa Int && !iszero(mod(period,review_period)) 
+        return false
+    elseif review_period isa Union{StepRange,Vector} && !in(period, review_period)
+        return false
+    else
+        return true
+    end
+end
+
+"""
+    check_policy_inputs!(
+        reorder_point::Dict, policy_param::Dict, review_period::Union{Int, StepRange, Vector, Dict}, 
+        min_order_qty::Union{Real, Dict}, mats::Vector, request_nodes::Vector, supply_nodes::Vector
+    )
+
+Validate inputs to inventory policy.
+"""
+function check_policy_inputs!(
+    reorder_point::Dict, policy_param::Dict, review_period::Union{Int, StepRange, Vector, Dict}, 
+    min_order_qty::Union{Real, Dict}, mats::Vector, request_nodes::Vector, supply_nodes::Vector
+)
+    #check inputs
+    for mat in mats
+        for n in request_nodes
+            for param in [reorder_point, policy_param] 
+                if !in((n,mat), keys(param)) 
+                    param[(n,mat)] = -1 #if no policy given for a node/material, set the params to -1 so that a reorder is never triggered
+                end
+            end
+            if review_period isa Dict && !in((n,mat), keys(review_period))
+                review_period[(n,mat)] = 1 #set to default value of 1
+            end
+        end
+        for n in supply_nodes
+            if min_order_qty isa Dict && !in((n,mat), keys(min_order_qty))
+                min_order_qty[(n,mat)] = 0 #set to default value of 0
+            end
+        end
+    end
+end
+
+"""
+    get_inventory_state(
+        n::Int, mat::Symbol, policy_variable::Union{Dict,Symbol},
+        state1_grp::GroupedDataFrame, state2_grp::GroupedDataFrame
+    )
+
+Check the inventory state at node `n` for material `mat`.
+"""
+function get_inventory_state(
+    n::Int, mat::Symbol, policy_variable::Union{Dict,Symbol},
+    state1_grp::GroupedDataFrame, state2_grp::GroupedDataFrame
+)
+    pol_var = policy_variable isa Dict ? policy_variable[n] : policy_variable
+    @assert pol_var in [:inventory_position, :echelon_stock] "Policy variable must be either `:inventory_position` or `:echelon_stock`."
+    if pol_var == :inventory_position
+        state = state1_grp[(node = n, material = mat)].level[1]
+    elseif pol_var == :echelon_stock
+        state = state2_grp[(node = n, material = mat)].level[1]
+    end
+
+    return state
+end
+
+"""
+    get_expected_consumption(env::SupplyChainEnv, n::Int, mat::Symbol, action::NamedArray)
+
+Get expected raw material consumption at the producer node for downstream requests.
+"""
+function get_expected_consumption(env::SupplyChainEnv, n::Int, mat::Symbol, action::NamedArray)
+    bom = get_prop(env.network, n, :bill_of_materials)
+    mprods = names(filter(i -> i < 0, bom[mat,:]), 1) #find which products are produced from mat
+    successors = outneighbors(env.network, n) #get successor nodes
+    consume = 0
+    for succ in successors, mprod in mprods
+        ordered = action[mprod, (n, succ)] #amount ordered by successor
+        stoich = bom[mat, mprod] #stoichiometry
+        consume += ordered * stoich #amount that will be consumed to fulfill the order (negative)
+    end
+
+    return consume
+end
+
+"""
+    calculate_reorder(
+        n::Int, mat::Symbol, state::Float64, MOQ::Real, 
+        policy_type::Union{Dict, Symbol}, policy_param::Dict
+    )
+
+Calculate reorder quantity for material `mat` at node `n`.
+"""
+function calculate_reorder(
+    n::Int, mat::Symbol, state::Float64, MOQ::Real, 
+    policy_type::Union{Dict, Symbol}, policy_param::Dict
+)
+    pol_type = policy_type isa Dict ? policy_type[n] : policy_type #policy type
+    @assert pol_type in [:rQ, :sS] "The policy type must be either `:rQ` or `:sS`."
+    if pol_type == :rQ #rQ policy
+        reorder = max(policy_param[n,mat], MOQ)
+    elseif pol_type == :sS #sS policy
+        reorder = state < policy_param[n,mat] ? max(policy_param[n,mat] - state, MOQ) : 0
+    end
+    
+    return reorder
 end
 
 """
@@ -113,10 +189,10 @@ Step through a simulation using a specified reorder policy. `args` are the
 arguments that are passed to the `reorder_policy` function.
 """
 function simulate_policy!(env::SupplyChainEnv, args...; kwargs...)
-    for t in 1:env.num_periods
+    for _ in 1:env.num_periods
         action = reorder_policy(env, args...; kwargs...)
         (env)(action)
     end
-    
+
     calculate_service_measures!(env)
 end
